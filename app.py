da55baeeac43c3be20bbf6934ad5045f5cbd6c82
Денисенко Александра <denisenko.sos@yandex.ru>
2025-09-07 23:54:36 +0700

    Счётчик

--- app.py
+++ app.py
@@ -37,3 +37,16 @@ def image():
                 <img src="''' + path + '''">
             </body>
         </html>'''
+
+count = 0
+
+@app.route('/counter')
+def counter():
+    global count
+    count += 1
+    return '''<!doctype html>
+        <html>
+            <body>
+                Сколько раз вы сюда заходили: ''' + str(count) + '''
+            </body>
+        </html>'''
