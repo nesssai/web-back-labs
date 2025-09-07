c64bb3b7e4c517d9201dc07cc7cbc1063428e674
Денисенко Александра <denisenko.sos@yandex.ru>
2025-09-08 01:59:34 +0700

    Стили

--- app.py
+++ app.py
@@ -38,11 +38,17 @@ def author():
 @app.route('/image')
 def image():
     path = url_for("static", filename="sakura.jpg")
+    css_path = url_for("static", filename="lab1.css")
     return '''<!doctype html>
         <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Сакура</title>
+                <link rel="stylesheet" href="''' + css_path + '''">
+            </head>
             <body>
                 <h1>Сакура</h1>
-                <img src="''' + path + '''">
+                <img class="sakura" src="''' + path + '''" alt="Сакура">
             </body>
         </html>'''
 
