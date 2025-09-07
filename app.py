5bd2416313b26b7977b965d2f89c07bf6f35c012
Денисенко Александра <denisenko.sos@yandex.ru>
2025-09-08 00:08:10 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -59,3 +59,7 @@ def counter():
                 Ваш IP адрес: ''' + client_ip + '''<br>
             </body>
         </html>'''
+
+@app.route('/info')
+def info():
+    return redirect("/author")
