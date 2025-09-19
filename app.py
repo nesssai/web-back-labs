9a60c96fe318f7492a4cbd80e5d9fc926c61adc8
Денисенко Александра <denisenko.sos@yandex.ru>
2025-09-19 15:27:57 +0700

    Имя студента

--- app.py
+++ app.py
@@ -351,4 +351,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Денисенко Александра Юрьевна'
+    return render_template('example.html', name=name)
