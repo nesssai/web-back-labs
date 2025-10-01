7634dd772d177cb1c32f7e5a5583bcae1fe0e81f
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-01 16:49:26 +0700

    Рефакторинг: выделение первой лабораторной

---
+++ lab1.py
@@ -0,0 +1,233 @@
+from flask import Blueprint, url_for, request, redirect
+import datetime
+lab1 = Blueprint('lab1', __name__)
+
+access_log = []
+
+
+@lab1.route('/lab1/')
+def lab():
+    return """<!doctype html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Лабораторная 1</title>
+            </head>
+            <body>
+                <p>
+                Flask &mdash; фреймворк для создания веб-приложений на языке программирования Python,
+                использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2.
+                Относится к категории так называемых микрофреймворков &mdash; минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+                </p>
+
+                <p><a href="/">На главную</a></p>
+
+                <h2>Список роутов:</h2>
+                <ul>
+                    <li><a href="/lab1/web">/lab1/web</a></li>
+                    <li><a href="/lab1/author">/lab1/author</a></li>
+                    <li><a href="/lab1/image">/lab1/image</a></li>
+                    <li><a href="/lab1/counter">/lab1/counter</a></li>
+                    <li><a href="/lab1/counter/clear">/lab1/counter/clear</a></li>
+                    <li><a href="/lab1/info">/lab1/info</a></li>
+                    <li><a href="/lab1/created">/lab1/created</a></li>
+                    <li><a href="/lab1/error/400">/lab1/error/400</a></li>
+                    <li><a href="/lab1/error/401">/lab1/error/401</a></li>
+                    <li><a href="/lab1/error/402">/lab1/error/402</a></li>
+                    <li><a href="/lab1/error/403">/lab1/error/403</a></li>
+                    <li><a href="/lab1/error/405">/lab1/error/405</a></li>
+                    <li><a href="/lab1/error/418">/lab1/error/418</a></li>
+                    <li><a href="/lab1/trigger500">/lab1/trigger500</a></li>
+                </ul>
+            </body>
+        </html>"""
+
+
+@lab1.route("/lab1/web")
+def web():
+    return """<!doctype html>
+        <html>
+            <body>
+               <h1>web-сервер на flask</h1>
+               <a href="/lab1/author">author<a>
+            </body>
+        </html>""", 200, {
+            'X-Server': 'sample',
+            'Content-Type': 'text/plain; charset=utf-8'
+            }
+
+
+@lab1.route("/lab1/author")
+def author():
+    name = "Денисенко Александра Юрьевна"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/lab1/web">web<a>
+            </body>
+        </html>"""
+
+
+@lab1.route('/lab1/image')
+def image():
+    path = url_for("static", filename="sakura.jpg")
+    css_path = url_for("static", filename="lab1.css")
+
+    html = '''<!doctype html>
+        <html>
+            <head>
+                <title>Сакура</title>
+                <link rel="stylesheet" href="''' + css_path + '''">
+            </head>
+            <body>
+                <h1>Сакура</h1>
+                <img class="sakura" src="''' + path + '''" alt="Сакура">
+            </body>
+        </html>'''
+    
+    headers = {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Content-Language': 'ru',
+        'X-Student-Name': 'Aleksandra Denisenko',
+        'X-Lab': 'Lab-1'
+    }
+    
+    return html, 200, headers
+
+count = 0
+
+
+@lab1.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+
+    return '''<!doctype html>
+        <html>
+            <body>
+                Сколько раз вы сюда заходили: ''' + str(count) + '''
+                <hr>
+                Дата и время: ''' + str(time) + '''<br>
+                Запрошенный адрес: ''' + url + '''<br>
+                Ваш IP адрес: ''' + client_ip + '''<br>
+                <hr>
+                <a href="/lab1/counter/clear">Очистить счётчик</a>
+            </body>
+        </html>'''
+
+
+@lab1.route('/lab1/counter/clear')
+def counter_clear():
+    global count
+    count = 0
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>Счётчик очищен</h1>
+                <p>Значение счётчика теперь равно 0</p>
+                <a href="/lab1/counter">Вернуться на страницу счётчика</a>
+            </body>
+        </html>'''
+
+
+@lab1.route('/lab1/info')
+def info():
+    return redirect("/lab1/author")
+
+
+@lab1.route("/lab1/created")
+def created():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>Создано успешно</h1>
+                <div><i>что-то создано...</i></div>
+            </body>
+        </html>''', 201
+
+
+@lab1.route('/lab1/error/400')
+def error_400():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>400 Bad Request</h1>
+                <p>Сервер не смог понять запрос из-за неверного синтаксиса.</p>
+            </body>
+        </html>''', 400, {'Content-Type': 'text/html; charset=utf-8'}
+
+
+@lab1.route('/lab1/error/401')
+def error_401():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>401 Unauthorized</h1>
+                <p>Требуются учетные данные для доступа к ресурсу.</p>
+            </body>
+        </html>''', 401, {
+            'Content-Type': 'text/html; charset=utf-8',
+            'WWW-Authenticate': 'Basic realm="Login Required"'
+            }
+
+
+@lab1.route('/lab1/error/402')
+def error_402():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>402 Payment Required</h1>
+                <p>Зарезервировано для будущего использования.</p>
+            </body>
+        </html>''', 402, {'Content-Type': 'text/html; charset=utf-8'}
+
+
+@lab1.route('/lab1/error/403')
+def error_403():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>403 Forbidden</h1>
+                <p>Клиент не имеет прав доступа к контенту.</p>
+            </body>
+        </html>''', 403, {'Content-Type': 'text/html; charset=utf-8'}
+
+
+@lab1.route('/lab1/error/405')
+def error_405():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>405 Method Not Allowed</h1>
+                <p>Используемый HTTP-метод не разрешён для данного ресурса.</p>
+            </body>
+        </html>''', 405, {
+            'Content-Type': 'text/html; charset=utf-8',
+            'Allow': 'GET'
+            }
+
+
+@lab1.route('/lab1/error/418')
+def error_418():
+    return '''<!doctype html>
+        <html>
+            <body>
+                <h1>418 I'm a teapot</h1>
+                <p>«Шуточный» ответ: сервер отклоняет попытку заварить кофе в чайнике.</p>
+            </body>
+        </html>''', 418, {'Content-Type': 'text/html; charset=utf-8'}
+
+
+@lab1.route('/lab1/trigger500')
+def trigger_500():
+    return 1 / 0
