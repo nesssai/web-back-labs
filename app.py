fd8fe0512f56f82807924cb865e3375b14f08600
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-15 18:21:57 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, url_for, request
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 import datetime
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 access_log = []
 
@@ -92,6 +94,7 @@ def index():
                         <li><a href="/lab1">Первая лабораторная</a></li>
                         <li><a href="/lab2/">Вторая лабораторная</a></li>
                         <li><a href="/lab3/">Третья лабораторная</a></li>
+                        <li><a href="/lab4/">Четвёртая лабораторная</a></li>
                     </ul>
                 </div>
 
