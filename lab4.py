16073deb103ad5335679e627c2dcdd30fb14e191
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-17 14:23:38 +0700

    Счётчик деревьев

--- lab4.py
+++ lab4.py
@@ -85,3 +85,20 @@ def power():
     
     result = a ** b
     return render_template('lab4/pow.html', x1=a, x2=b, result=result)
+
+tree_count = 0
+
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+
+    if operation == 'cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count += 1
+        
+    return render_template('lab4/tree.html', tree_count=tree_count)
