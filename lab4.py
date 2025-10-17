c8c74ecfa093817d25fe3833396d5f745fa82a7a
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-17 13:58:21 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -16,14 +16,72 @@ def div():
     if x1 == '' or x2 == '':
         return render_template('lab4/div.html', error='Оба поля должны быть заполнены!')
     
-    try:
-        x1 = int(x1)
-        x2 = int(x2)
-    except ValueError:
-        return render_template('lab4/div.html', error='Введите целые числа!')
-
+    x2 = int(x2)
     if x2 == 0:
         return render_template('lab4/div.html', error='На ноль делить нельзя!')
 
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/add-form')
+def add_form():
+    return render_template('lab4/add-form.html')
+
+@lab4.route('/lab4/add', methods=['POST'])
+def add():
+    x1 = request.form.get('x1', '')
+    x2 = request.form.get('x2', '')
+    
+    a = float(x1) if x1.strip() != '' else 0.0
+    b = float(x2) if x2.strip() != '' else 0.0
+    result = a + b
+    return render_template('lab4/add.html', x1=a, x2=b, result=result)
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1', '')
+    x2 = request.form.get('x2', '')
+    
+    a = float(x1) if x1.strip() != '' else 1.0
+    b = float(x2) if x2.strip() != '' else 1.0
+    result = a * b
+    return render_template('lab4/mul.html', x1=a, x2=b, result=result)
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1', '')
+    x2 = request.form.get('x2', '')
+    if x1.strip() == '' or x2.strip() == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены')
+    
+    a = float(x1)
+    b = float(x2)
+    result = a - b
+    return render_template('lab4/sub.html', x1=a, x2=b, result=result)
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def power():
+    x1 = request.form.get('x1', '')
+    x2 = request.form.get('x2', '')
+    if x1.strip() == '' or x2.strip() == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены')
+    
+    a = float(x1)
+    b = float(x2)
+    if a == 0.0 and b == 0.0:
+        return render_template('lab4/pow.html', error='0^0 не определено')
+    
+    result = a ** b
+    return render_template('lab4/pow.html', x1=a, x2=b, result=result)
