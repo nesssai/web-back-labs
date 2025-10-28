ac8b5ba6c2ab40295ff6df3d05da5959b369c284
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-28 19:56:22 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -148,3 +148,34 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    error = None
+    message = None
+    snowflakes = 0
+    temp_str = ''
+
+    if request.method == 'POST':
+        temp_str = request.form.get('temperature')
+
+        if not temp_str:
+            error = "Ошибка: не задана температура"
+        else:
+            temp = int(temp_str) 
+
+            if temp < -12:
+                error = "Не удалось установить температуру - слишком низкое значение"
+            elif temp > -1:
+                error = "Не удалось установить температуру - слишком высокое значение"
+            elif -12 <= temp <= -9:
+                message = f"Установлена температура: {temp}°С"
+                snowflakes = 3
+            elif -8 <= temp <= -5:
+                message = f"Установлена температура: {temp}°С"
+                snowflakes = 2
+            elif -4 <= temp <= -1:
+                message = f"Установлена температура: {temp}°С"
+                snowflakes = 1
+
+    return render_template('lab4/fridge.html', error=error, message=message, snowflakes=snowflakes, temp=temp_str)
