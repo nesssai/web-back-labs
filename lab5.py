fa6cc993c211b03514b559d053fa84924a938f1c
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-30 09:23:52 +0700

    Удаление дублирующегося кода

--- lab5.py
+++ lab5.py
@@ -10,6 +10,22 @@ def lab():
 
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='sasha_denisenko_knowledge_base',
+        user='sasha_denisenko_knowledge_base',
+        password='sasha12'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -21,25 +37,17 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host='127.0.0.1',
-        database='sasha_denisenko_knowledge_base',
-        user='sasha_denisenko_knowledge_base',
-        password='sasha12'
-    )
-
-    cur = conn.cursor()
+    conn, cur =  db_connect()
 
     cur.execute(f"SELECT login FROM users WHERE login = '{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+
+    db_close(conn, cur)
+
     return render_template('lab5/success.html', login=login)
 
 @lab5.route('/lab5/login', methods=['GET', 'POST'])
@@ -53,30 +61,19 @@ def login():
     if not (login or password):
         return render_template('lab5/login.html', error="Заполните поля")
     
-    conn = psycopg2.connect(
-        host='127.0.0.1',
-        database='sasha_denisenko_knowledge_base',
-        user='sasha_denisenko_knowledge_base',
-        password='sasha12'
-    )
-
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    conn, cur =  db_connect()
 
     cur.execute(f"SELECT * FROM users WHERE login='{login}'")
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
 
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
     session['login'] = login
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
-
