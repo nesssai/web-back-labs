44f6dfa95a72bff877bd9b8ebe72923dd163b217
Денисенко Александра <denisenko.sos@yandex.ru>
2025-10-30 09:50:24 +0700

    Хеширование паролей

--- lab5.py
+++ lab5.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import generate_password_hash, check_password_hash
 
 lab5 = Blueprint('lab5', __name__)
 
@@ -25,7 +26,6 @@ def db_close(conn, cur):
     cur.close()
     conn.close()
 
-
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -44,7 +44,8 @@ def register():
         db_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    password_hash = generate_password_hash(password)
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
 
     db_close(conn, cur)
 
@@ -70,7 +71,7 @@ def login():
         db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
 
-    if user['password'] != password:
+    if not check_password_hash(user['password'], password):
         db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
