900185bd9256a43f47d8b0bf4029c5db6a922bc0
Денисенко Александра <denisenko.sos@yandex.ru>
2025-11-14 00:13:46 +0700

    Стоимость аренды

--- lab6.py
+++ lab6.py
@@ -4,11 +4,11 @@ lab6 = Blueprint('lab6', __name__)
 
 offices = []
 for i in range(1, 11):
-    offices.append({"number": i, "tenant": ""})
+    offices.append({"number": i, "tenant": "", "price": 900 + i * 100})
 
 @lab6.route('/lab6/')
 def lab():
-    return render_template('lab6/lab6.html')
+    return render_template('lab6/lab6.html', current_user=session.get('login'))
 
 @lab6.route('/lab6/json-rpc-api/', methods=['POST'])
 def api():
