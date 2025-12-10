712f11ecbfc49759433d0237eee95f501313fb01
Денисенко Александра <denisenko.sos@yandex.ru>
2025-12-10 22:10:31 +0700

    Авторизация

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from db import db
+from db.models import users
+from flask_login import LoginManager
 
 from lab1 import lab1
 from lab2 import lab2
@@ -15,6 +17,14 @@ import os
 from os import path
 import datetime
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
