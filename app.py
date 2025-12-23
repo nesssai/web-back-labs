a7140769b499ca0549519f73042de3b7eae8c8a7
Денисенко Александра <denisenko.sos@yandex.ru>
2025-12-24 04:54:14 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz
 
 import os
@@ -57,6 +58,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz)
 
 access_log = []
@@ -147,6 +149,7 @@ def index():
                         <li><a href="/lab6/">Шестая лабораторная</a></li>
                         <li><a href="/lab7/">Седьмая лабораторная</a></li>
                         <li><a href="/lab8/">Восьмая лабораторная</a></li>
+                        <li><a href="/lab9/">Девятая лабораторная</a></li>
                         <li><a href="/rgz/">Расчетно-графическое задание</a></li>
                     </ul>
                 </div>
